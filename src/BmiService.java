8eab40c701730a37872b746abb69d0888640850b
Марина Волкова <marvolkova.mv@gmail.com>
2023-02-23 18:48:24 +0200

    new commit 2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weight, double height) {
-        double BMI = weight / height;
+        double BMI = weight / (height*height);
         return (int) BMI;
     }
 }
