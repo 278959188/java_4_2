b2022403a9c9ccca9a6e487e5913346e5faa28ae
278959188 <122278987+278959188@users.noreply.github.com>
2023-02-27 20:17:47 +0200

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int weight, double height) {
-        double BMI = weight / (height*height);
-        return (int) BMI;
+        double bmi = weight / (height * height);
+        return (int) bmi;
     }
 }
